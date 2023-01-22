0ceaeb9f7734d42be438a5baad126762341353b8
Carlos Junior <carlos_junior__cod@hotmail.com>
2023-01-22 15:46:32 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//alert("Hello world")
+
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() { 
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert('Dia já incluso')
+        return
+    }
+
+
+    alert('Adicionado com sucesso')
+    nlwSetup.addDay(today)
+}
+
+function save() {
+    localStorage.setItem('Habits_saves', JSON.stringify(nlwSetup.data))
+}
+const data = JSON.parse(localStorage.getItem('Habits_saves'))  || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
